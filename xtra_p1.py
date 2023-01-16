4ab3b0a98d6a6f88c897d96f0662f41d9599878b
Kendall Outlaw <116039680+kenoutlaw@users.noreply.github.com>
2023-01-16 18:49:16 -0500

    commit changes

--- xtra_p1.py
+++ xtra_p1.py
@@ -7,12 +7,13 @@ import random
 
 # Change the name below to a name of your choice
 
-name = "GameBot"
+name = "Gameboy"
 
 # Fix the code below to print the name using an f-string
 
+
 print()
-print("Hello, I'm {name}, your gamebot.")
+print("Hello, I'm ", name, " your gamebot.")
 print("Let's play an animal guessing game!")
 print("There are 3 animals: wolf, eagle, snake (a Python of course).")
 print("The wolf scares the eagle.")
@@ -26,7 +27,9 @@ print()
 # enter wolf, eagle, or snake.
 # Hint: use the input() function
 
-user_choice = "wolf"
+
+user_choice = input('Enter wolf, eagle, or snake')
+
 
 # Now the bot will pick one
 buddy_choice = random.choice(["wolf", "eagle", "snake"])
@@ -45,10 +48,42 @@ print()
 if user_choice == buddy_choice:
     print("We tied!")
 
+if user_choice != buddy_choice:
+    print("We chose different animals!")
+
+
+if user_choice == "eagle" or buddy_choice == "eagle" and user_choice == "wolf" or buddy_choice == "wolf":
+    print("The wolf scares the eagle.")
+
+    if user_choice == "eagle":
+        print("I win")
+
+    if user_choice == "eagle":
+        print("You win")
+
+
+if user_choice == "eagle" or buddy_choice == "eagle" and user_choice == "snake" or buddy_choice == "snake":
+    print("The eagle grabs the snake.")
+
+    if user_choice == "eagle":
+        print("You win")
+
+    if buddy_choice == "eagle":
+        print("I win")
+
+
+if user_choice == "snake" or buddy_choice == "snake" and user_choice == "wolf" or buddy_choice == "wolf":
+    print("The snake bites the wolf.")
+
+    if user_choice == "snake":
+        print("You win")
+
+    if buddy_choice == "snake":
+        print("I win")
 
 # When you finish,
 # right-click on the code and select "Format Document"
 
 # Run the code, and play the game a few times.
-# Copy the output from the terminal and paste it into 
+# Copy the output from the terminal and paste it into
 # a new file named xtra_p1out.txt.
